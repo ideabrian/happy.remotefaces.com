b309d02443d6af8ffe47d02d53cacc3ad8fa2a58
Patrick Lorenzut <patrick.lorenzut@gmail.com>
2020-04-06 09:39:21 -0700

    another confetti fix

--- pages/index.vue
+++ pages/index.vue
@@ -63,14 +63,25 @@ export default {
     methods: {
         stopConfetti(){
             this.$confetti.stop();
+        },
+        startConfetti(){
+            this.$confetti.start();
+            if(this.$store.state.confetti){
+                this.$confetti.start();
+                setTimeout(this.stopConfetti, 3000)
+                this.$store.commit('SET_CONFETTI', false)
+            }
         }
     },
     mounted(){
-        if(this.$store.state.confetti){
-            this.$confetti.start();
-            setTimeout(this.stopConfetti, 3000)
-            this.$store.commit('SET_CONFETTI', false)
+        if(this.room){
+            this.startConfetti()            
         }
-    }
+    },
+    watch: {
+        room() {
+            this.startConfetti()
+        }
+    }  
 }
 </script>
